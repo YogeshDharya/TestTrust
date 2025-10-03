c453c35bc1628d7ef0e7d3e6a94b8244c942d1b3
YogeshDharya <yogeshdharya@gmail.com>
2025-10-03 11:47:28 +0530

    Got the repositories right

--- src/main/java/com/example/geektrust/Main.java
+++ src/main/java/com/example/geektrust/Main.java
@@ -6,17 +6,23 @@ import java.io.FileReader;
 import com.example.geektrust.command.Command;
 import com.example.geektrust.exception.MetroException;
 import com.example.geektrust.factory.CommandFactory;
+import com.example.geektrust.repository.CardRepository;
+import com.example.geektrust.repository.StationRepository;
+import com.example.geektrust.repository.impl.CardRepositoryImpl;
+import com.example.geektrust.repository.impl.StationRepositoryImpl;
 import com.example.geektrust.service.impl.MetroServiceImpl;
 
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        CardRepository cardRepository = CardRepositoryImpl.getInstance();
+        StationRepository stationRepository = StationRepositoryImpl.getInstance();
         if (args.length < 1) {
             System.out.println("Input file required");
             return;
         }
-        MetroServiceImpl service = MetroServiceImpl.getInstance();
+        MetroServiceImpl service = MetroServiceImpl.getInstance(cardRepository,stationRepository);
         try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
             String line;
             while ((line = br.readLine()) != null) {
