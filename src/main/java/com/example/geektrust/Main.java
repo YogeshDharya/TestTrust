e2474cfe477d1293aa2e40aea9080b0e670c317b
YogeshDharya <yogeshdharya@gmail.com>
2025-10-03 11:41:48 +0530

    Modified Main class

--- src/main/java/com/example/geektrust/Main.java
+++ src/main/java/com/example/geektrust/Main.java
@@ -1,24 +1,35 @@
 package com.example.geektrust;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import com.example.geektrust.command.Command;
+import com.example.geektrust.exception.MetroException;
+import com.example.geektrust.factory.CommandFactory;
+import com.example.geektrust.service.impl.MetroServiceImpl;
+
 
 public class Main {
     public static void main(String[] args) {
         
-//        Sample code to read from file passed as command line argument
-        try {
-            // the file to be opened for reading
-            FileInputStream fis = new FileInputStream(args[0]);
-            Scanner sc = new Scanner(fis); // file to be scanned
-            // returns true if there is another line to read
-            while (sc.hasNextLine()) {
-               //Add your code here to process input commands
-            }
-            sc.close(); // closes the scanner
-        } catch (IOException e) {
+        if (args.length < 1) {
+            System.out.println("Input file required");
+            return;
         }
-
+        MetroServiceImpl service = MetroServiceImpl.getInstance();
+        try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                try {
+                    String[] parts = line.split(" ");
+                    Command command = CommandFactory.createCommand(parts[0], parts);
+                    command.execute(service, parts);
+                } catch (MetroException e) {
+                    System.err.println("Error processing command: " + e.getMessage());
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
     }
 }
