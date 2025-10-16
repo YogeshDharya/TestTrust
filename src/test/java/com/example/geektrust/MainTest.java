06a78b59e1f01eb307d1e1b621c090637d40cba9
YogeshDharya <yogeshdharya@gmail.com>
2025-10-16 10:33:59 +0530

    Testing main class

--- src/test/java/com/example/geektrust/MainTest.java
+++ src/test/java/com/example/geektrust/MainTest.java
@@ -1,4 +1,11 @@
 package com.example.geektrust;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MainTest {
+        @Test
+    public void Application_Test() throws Exception{
+        Assertions.assertTrue(true);
+    }
 }
